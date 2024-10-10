4a65f9bb529053e4b4a73a1e630bb4f41bf05a0b
allison-0 <allisonulrich00@gmail.com>
2024-10-09 23:07:15 -0700

    commit

--- olympic_rings.py
+++ olympic_rings.py
@@ -3,6 +3,9 @@ import turtle
 def draw_circle(t, x, y, color):
     t.penup() # Lift pen to move without drawing anything
     t.setpos(x, y - 45) # Move to the bottom of the circle
+    t.pendown() # Start drawing
+    t.color(color) # Make color generic for flexibility
+    t.circle(45) # Draw circle with a radius of 45
 
 
 def main():
