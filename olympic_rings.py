ff4e650f1054ef2a62d34b290719ff530d234d47
allison-0 <allisonulrich00@gmail.com>
2024-10-09 23:10:15 -0700

    commit

--- olympic_rings.py
+++ olympic_rings.py
@@ -7,8 +7,11 @@ def draw_circle(t, x, y, color):
     t.color(color) # Make color generic for flexibility
     t.circle(45) # Draw circle with a radius of 45
 
-
 def main():
+    t = turtle.Turtle() # Create turtle
+    t.width(5) # Set line width
+    t.speed(6) # Make the animation a bit faster
+    t.hideturtle() # Hide turtle cursor
 
 if __name__ == "__main__":
     main()
