097dba2c685bc7b14a129ca05f21c6d13fd2cca1
allison-0 <allisonulrich00@gmail.com>
2024-10-09 23:03:54 -0700

    commit

--- olympic_rings.py
+++ olympic_rings.py
@@ -1,6 +1,9 @@
 import turtle
 
 def draw_circle(t, x, y, color):
+    t.penup() # Lift pen to move without drawing anything
+    t.setpos(x, y - 45) # Move to the bottom of the circle
+
 
 def main():
 
